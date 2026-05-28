94d94162bccb57877da6d11bb4b79b3434f121e2
Newraiwa <paweenkit.t@ku.th>
2026-05-28 15:58:59 +0700

    auth register and login

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,65 @@
+const prisma = require("../config/db");
+const bcrypt = require("bcryptjs");
+const generateToken = require("../utils/generateToken");
+
+exports.register = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        username,
+        password: hashedPassword,
+      },
+    });
+
+    res.json({
+      message: "Register Success",
+      token: generateToken(user.id),
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        username,
+      },
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        message: "User not found",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!isMatch) {
+      return res.status(401).json({
+        message: "Invalid password",
+      });
+    }
+
+    res.json({
+      message: "Login Success",
+      token: generateToken(user.id),
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
